419d7f4cbd88a55aa5aa05bb025226874d4f0651
uvsqNathanAmoussou <nathan.amoussou@ens.uvsq.fr>
2022-02-08 10:22:30 +0100

    changement mineur

--- tas_de_sable.py
+++ tas_de_sable.py
@@ -2,7 +2,7 @@
 # groupe MI 2
 # Kamila DIMIA
 # Marlone BERTUIT
-# Nathan AMOUSSOU
+# Nathan AMOUSSOU *
 # https://github.com/uvsqNathanAmoussou/projet_tas_de_sable
 #########################################
 
