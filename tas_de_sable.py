5cbff8fd0c9deea7be64b1ca7203af31d1c9b2b3
uvsqNathanAmoussou <nathan.amoussou@ens.uvsq.fr>
2022-02-08 10:14:38 +0100

    création de tas_de_sable.py

---
+++ tas_de_sable.py
@@ -0,0 +1,7 @@
+#########################################
+# groupe MI 2
+# Toto LEHERO
+# Titi LEVAINQUEUR
+# etc...
+# https://github.com/uvsq-info/l1-python
+#########################################
